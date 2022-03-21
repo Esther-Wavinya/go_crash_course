27ca685e3941ec1419cdb2164491803499b4ee1c
Esther-Wavinya <estherwavinya80@gmail..com>
2022-03-21 12:04:34 +0300

    remainingoventime

--- go exercism algorithms/Gopher's _Gorgeous _Lasagna.go
+++ go exercism algorithms/Gopher's _Gorgeous _Lasagna.go
@@ -22,8 +22,12 @@
 // If you see a syntax error: unexpected :=, expecting = error then you are likely trying to assign the constant using := like a variable; constants are assigned using = not :=.
 
 // TODO: define the 'OvenTime' constant
+package lasagna
 
 const OvenTime = 40
+
+
+
 
 
 // 2. Calculate the remaining oven time in minutes
@@ -35,6 +39,26 @@ const OvenTime = 40
 
 // RemainingOvenTime(30)
 // // Output: 10
+// Calculate the remaining oven time in minutes
+// You need to define a function with a single parameter.
+
+// You have to explicitly return an integer from a function.
+
+// The function's parameter is an integer.
+
+// You can call one of the other functions you've defined previously.
+
+// You can use the mathematical operator for subtraction to subtract values.
+
+// RemainingOvenTime returns the remaining minutes based on the `actual` minutes already in the oven.
+func RemainingOvenTime(actualMinutesInOven int) int {
+    return(OvenTime - actualMinutesInOven)	
+}
+
+
+
+
+
 // 3. Calculate the preparation time in minutes
 // Define the PreparationTime function that takes the number of layers you added to the lasagna as a parameter and returns how many minutes you spent preparing the lasagna, assuming each layer takes you 2 minutes to prepare.
 
@@ -43,7 +67,26 @@ const OvenTime = 40
 // }
 
 // PreparationTime(2)
-// // Output: 4
+// // // Output: 4
+// Calculate the preparation time in minutes
+// You need to define a function with a single parameter.
+
+// You have to explicitly return an integer from a function.
+
+// The function's parameter is an integer.
+
+// You can use the mathematical operator for multiplication to multiply values.
+
+
+// PreparationTime calculates the time needed to prepare the lasagna based on the amount of layers.
+func PreparationTime(numberOfLayers int) int {
+    return(numberOfLayers * 2)	
+}
+
+
+
+
+
 // 4. Calculate the elapsed working time in minutes
 // Define the ElapsedTime function that takes two parameters: the first parameter is the number of layers you added to the lasagna, and the second parameter is the number of minutes the lasagna has been in the oven. The function should return how many minutes in total you've worked on cooking the lasagna, which is the sum of the preparation time in minutes, and the time in minutes the lasagna has spent in the oven at the moment.
 
@@ -53,3 +96,76 @@ const OvenTime = 40
 
 // ElapsedTime(3, 20)
 // // Output: 26
+
+// Calculate the elapsed working time in minutes
+// You need to define a function with two parameters.
+
+// You have to explicitly return an integer from a function.
+
+// The function's parameter is an integer.
+
+// You can call one of the other functions you've defined previously.
+
+// You can use the mathematical operator for addition to add values.
+
+// ElapsedTime calculates the total time needed to create and bake a lasagna.
+func ElapsedTime(numberOfLayers, actualMinutesInOven int) int {
+	return(PreparationTime(numberOfLayers) + actualMinutesInOven)
+ }
+ 
+
+
+
+
+
+
+
+
+//  Introduction
+// Go is a statically typed, compiled programming language. This exercise introduces three major language features: Packages, Functions, and Variables.
+
+// Packages
+// Go applications are organized in packages. A package is a collection of source files located in the same directory. All source files in a directory must share the same package name. When a package is imported, only entities (functions, types, variables, constants) whose names start with a capital letter can be used / accessed. The recommended style of naming in Go is that identifiers will be named using camelCase, except for those meant to be accessible across packages which should be PascalCase.
+
+// package lasagna
+// Variables
+// Go is statically-typed, which means all variables must have a defined type at compile-time.
+
+// Variables can be defined by explicitly specifying a type:
+
+// var explicit int // Explicitly typed
+// You can also use an initializer, and the compiler will assign the variable type to match the type of the initializer.
+
+// implicit := 10   // Implicitly typed as an int
+// Once declared, variables can be assigned values using the = operator. Once declared, a variable's type can never change.
+
+// count := 1 // Assign initial value
+// count = 2  // Update to new value
+
+// count = false // This throws a compiler error due to assigning a non `int` type
+// Constants
+// Constants hold a piece of data just like variables, but their value cannot change during the execution of the program.
+
+// Constants are defined using the const keyword and can be numbers, characters, strings or booleans:
+
+// const Age = 21 // Defines a numeric constant 'Age' with the value of 21
+// Functions
+// Go functions accept zero or more parameters. Parameters must be explicitly typed, there is no type inference.
+
+// Values are returned from functions using the return keyword.
+
+// A function is invoked by specifying the function name and passing arguments for each of the function's parameters.
+
+// Note that Go supports two types of comments. Single line comments are preceded by // and multiline comments are inserted between /* and */.
+
+// package greeting
+
+// // Hello is a public function
+// func Hello (name string) string {
+//     return hi(name)
+// }
+
+// // hi is a private function
+// func hi (name string) string {
+//     return "hi " + name
+// }
