4d9a530b672a90aad937c7e2bd5e56f48d693624
Esther-Wavinya <estherwavinya80@gmail..com>
2022-03-23 09:31:56 +0300

    cost

--- go exercism algorithms/cars_assemble.go
+++ go exercism algorithms/cars_assemble.go
@@ -56,3 +56,26 @@ func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int 
 
 
 
+// Task 3
+// Calculate the cost of production
+
+// Each car normally costs $10,000 to produce individually, regardless of whether it is successful or not. But with a bit of planning, 10 cars can be produced together for $95,000.
+
+// For example, 37 cars can be produced in the following way: 37 = 3 x groups of ten + 7 individual cars
+
+// So the cost for 37 cars is: 3*95,000+7*10,000=355,000
+
+// Implement the function CalculateCost that calculates the cost of producing a number of cars, regardless of whether they are successful:
+
+// cost := CalculateCost(37)
+// // Output: 355000
+
+// cost = CalculateCost(21)
+// // Output: 200000
+// Note: the return value should be an uint.
+
+
+// CalculateCost works out the cost of producing the given number of cars
+func CalculateCost(carsCount int) uint {
+    return((uint(carsCount / 10) * 95000) + (uint(carsCount % 10) * 10000 ))    
+}
