0b99b27a11a044d87b92c89b4f9981e0664aa643
Esther-Wavinya <estherwavinya80@gmail..com>
2022-06-10 09:35:22 +0300

    packages

--- codecademy/packages.go
+++ codecademy/packages.go
@@ -16,3 +16,17 @@ import "fmt"
 func main() {
 	fmt.Println("Hello World")
 }
+
+// main Function
+// We use the func keyword to declare the Go function main:
+
+// The func keyword denotes the start of a function declaration.
+// func is followed by the name of the function: main.
+// After the name there follows a pair of parentheses () and a set of curly braces {}.
+// The function code is written inside the set of curly braces.
+
+// Function Code
+// The code inside a function is indented. The code here invokes the Println function in the fmt package (that we imported earlier) to print the message "Hello World".
+
+// Invoking Functions
+// Normally when we write functions, you need to write code to invoke them, otherwise they are unused. However, the main function is different if it resides in the main package.
